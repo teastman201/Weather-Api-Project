2e43695d451e0ebc4f1df3546ffcd92c01a7e1bd
Tristan Eastman <teastman85@gmail.com>
2020-08-23 15:14:34 -0400

    Add working city search history.

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,37 @@
 var curDay = moment().format(" L ");
 
-    var key1 = JSON.parse(localStorage.getItem('city1', ''));
-    var key2 = JSON.parse(localStorage.getItem('city2', ''));
-    var key3 = JSON.parse(localStorage.getItem('city3', ''));
-    var key4 = JSON.parse(localStorage.getItem('city4', ''));
-    var key5 = JSON.parse(localStorage.getItem('city5', ''));
-    var key6 = JSON.parse(localStorage.getItem('city6', ''));
-    var key7 = JSON.parse(localStorage.getItem('city7', ''));
-    var key8 = JSON.parse(localStorage.getItem('city8', ''));
-    $(".list-group-city1").text(key1);
-    $(".list-group-city2").text(key2);
-    $(".list-group-city3").text(key3);
-    $(".list-group-city4").text(key4);
-    $(".list-group-city5").text(key5);
-    $(".list-group-city6").text(key6);
-    $(".list-group-city7").text(key7);
-    $(".list-group-city8").text(key8);
-    localStorage.setItem('city1', JSON.stringify(''));
-    localStorage.setItem('city2', JSON.stringify(''));
-    localStorage.setItem('city3', JSON.stringify(''));
-    localStorage.setItem('city4', JSON.stringify(''));
-    localStorage.setItem('city5', JSON.stringify(''));
-    localStorage.setItem('city6', JSON.stringify(''));
-    localStorage.setItem('city7', JSON.stringify(''));
-    localStorage.setItem('city8', JSON.stringify(''));
+// var key1 = JSON.parse(localStorage.getItem('city', [0]));
+// var key2 = JSON.parse(localStorage.getItem('city', [1]));
+// var key3 = JSON.parse(localStorage.getItem('city', [2]));
+// var key4 = JSON.parse(localStorage.getItem('city', [3]));
+// var key5 = JSON.parse(localStorage.getItem('city', [4]));
+// var key6 = JSON.parse(localStorage.getItem('city', [5]));
+// var key7 = JSON.parse(localStorage.getItem('city', [6]));
+// var key8 = JSON.parse(localStorage.getItem('city', [7]));
+
+
+// localStorage.setItem('city1', JSON.stringify(''));
+// localStorage.setItem('city2', JSON.stringify(''));
+// localStorage.setItem('city3', JSON.stringify(''));
+// localStorage.setItem('city4', JSON.stringify(''));
+// localStorage.setItem('city5', JSON.stringify(''));
+// localStorage.setItem('city6', JSON.stringify(''));
+// localStorage.setItem('city7', JSON.stringify(''));
+// localStorage.setItem('city8', JSON.stringify(''));
+retrievedData = localStorage.getItem("city", "value");
+var retrievedKeys = JSON.parse(retrievedData);
+// console.log(retrievedData);
+var arrayOfKeys = [];
+$(".list-group-city1").text(retrievedKeys[0]);
+$(".list-group-city2").text(retrievedKeys[1]);
+$(".list-group-city3").text(retrievedKeys[2]);
+$(".list-group-city4").text(retrievedKeys[3]);
+$(".list-group-city5").text(retrievedKeys[4]);
+$(".list-group-city6").text(retrievedKeys[5]);
+$(".list-group-city7").text(retrievedKeys[6]);
+$(".list-group-city8").text(retrievedKeys[7]);
+
+
 
 
 // Performing GET requests to the Open Weather API and logging the responses to the console
@@ -33,15 +41,23 @@ $('button').click(function () {
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=742e68c932f3e4e2f733f5a8c104a762";
   var queryUrlMulti = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=742e68c932f3e4e2f733f5a8c104a762";
 
-  for (h=1; h<9; h++){
-    
+  for (h = 1; h < 9; h++) {
+
     // $(".list-group-city").append("<li>");
     // $("<li>").addClass("list-group-city" + [h]);
     // $("<ul>").addClass("list-group list-group-flush" + [i]);
   }
-  
+
   // console.log(city);
-  
+  $(".list-group-city1").text(retrievedKeys[0]);
+$(".list-group-city2").text(retrievedKeys[1]);
+$(".list-group-city3").text(retrievedKeys[2]);
+$(".list-group-city4").text(retrievedKeys[3]);
+$(".list-group-city5").text(retrievedKeys[4]);
+$(".list-group-city6").text(retrievedKeys[5]);
+$(".list-group-city7").text(retrievedKeys[6]);
+$(".list-group-city8").text(retrievedKeys[7]);
+
   city = $("input").val();
 
   function searchCityWeather(city) {
@@ -53,14 +69,19 @@ $('button').click(function () {
   // console.log(queryURL);
 
   city = searchCityWeather(encodeURIComponent(city));
-  
+
+
+
 
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function (response) {
-    localStorage.setItem('city1', JSON.stringify(response.name));
-    console.log('');
+    
+    arrayOfKeys.push(response.name);
+    localStorage.setItem('city', JSON.stringify(arrayOfKeys));
+    console.log(arrayOfKeys);
+    // console.log('');
 
     var lat = response.coord.lat;
     var lon = response.coord.lon;
@@ -73,25 +94,18 @@ $('button').click(function () {
     $(".card-text-temp").text("Temperature: " + tempFah.toFixed(0));
     $(".card-text-wind").text("Windspeed: " + response.wind.speed);
     $(".card-text-hum").text("Humidity: " + response.main.humidity);
-    
-
 
     // Code for UV-index     
   });
 
-  
-  
+
+
   $.ajax({
     url: queryUrlMulti,
     method: "GET"
   }).then(function (responseMulti) {
 
     var arrayOfDays = responseMulti.list;
-
-
-    // for (m = 1; m < 6; m++) {  
-
-    // }
 
     var x = 0;
 
@@ -125,9 +139,9 @@ $('button').click(function () {
     }
 
   });
-  // localStorage.setItem('city1', JSON.stringify($('.city-input').val()));
-  
+
+
   document.getElementById('city-input').value = "";
-  
-  
+
+
 });
